60556f002a32c281985f8519be2cd757ccaae289
Mahmut164 <mahmutcift046@gmail.com>
2026-07-08 01:41:21 +0300

    Initialize Discord bot with basic commands

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`${client.user.tag} aktif!`);
+});
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === "!ping") {
+        message.reply("🏓 Pong!");
+    }
+});
+
+client.login(process.env.TOKEN);
